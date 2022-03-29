7d157f7195bc663e6324f6287efd19c9a10a209f
Yellow <yellowbuq@gmail.com>
2022-03-29 23:47:07 +0200

    added corrections to exercise 4

--- Próbna 2022/Liczby lustrzane (zadanie 4)/Liczby.py
+++ Próbna 2022/Liczby lustrzane (zadanie 4)/Liczby.py
@@ -28,12 +28,18 @@ for liczba in plik.readlines():
 if Zad_1:
 #Zadanie 4.1
     zlicz = 0
+    liczby_lustrzane =[]
 
     for x in range(0,2999):
-        for y in range(1,3000):
+        for y in range(x+1,3000):
             liczba1 = liczby[x][::-1]
             liczba2 = liczby[y]
-            if int(liczba1) == int(liczba2): zlicz = zlicz + 1
+            if liczba1[-1] != '0' and liczba1[0] != '0':
+                if int(liczba1) == int(liczba2):
+                    liczby_lustrzane.append((liczba1, liczba2))
+
+    liczby_lustrzanee = sorted(set(liczby_lustrzane))
+    zlicz = len(liczby_lustrzanee)
 
     print('Zad4.1: ', zlicz)
 
@@ -43,35 +49,40 @@ if Zad_2:
     pary = []
 
     for x in range(3000):
-        liczba1 = int(liczby[x])
-        liczba2 = int(liczby[x][::-1])
+        liczba1 = liczby[x][::-1]
+        liczba2 = liczby[x]
 
-        if czy_pierwsza(liczba1) and czy_pierwsza(liczba2):
-            if liczba1 > liczba2: pary.append((liczba2, liczba1))
-            else: pary.append((liczba1, liczba2))
+        if int(liczba2) > 10:
+            if liczba1[-1] != '0' and liczba1[0] != '0':
+                if czy_pierwsza(int(liczba1)) and czy_pierwsza(int(liczba2)):
+                    if liczba1 > liczba2: pary.append((liczba2, liczba1))
+                    else: pary.append((liczba1, liczba2))
 
-    print('Zad4.2: ', len(sorted(set(pary))), sorted(set(pary)))
+    print('Zad4.2: ', len(sorted(set(pary))))
 
 if Zad_3:
 #Zadanie 4.3
 
-    Liczby = []
+    liczby_połączone = []
 
     for i in liczby:
-        Liczby.append((int(i+i[::-1]),nwd(int(i+i[::-1]),int(i)),nwd(int(i+i[::-1]),int(i[::-1]))))
+        liczby_połączone.append((int(i+i[::-1])))
 
-    print('Zad4.3: ', sorted(set(Liczby)))
+    pierwsza = liczby_połączone[0]
+    for i in liczby_połączone:
+        pierwsza = nwd(pierwsza, i)
+
+    print('Zad4.3: ', pierwsza)
 
 if Zad_4:
 #Zadanie 4.4
 
-    Liczby = []
+    suma_liczb = []
     zlicz = 0
 
     for i in liczby:
-        Liczby.append(int(i)+int(i[::-1]))
-
-    Liczby = sorted(set(Liczby))
+        if int(i) > 10 or int(i[::-1]) > 10:
+            suma_liczb.append(int(i)+int(i[::-1]))
 
     kolejne_liczby_trójkątne = []
 
@@ -79,7 +90,7 @@ if Zad_4:
         suma = (i*(i+1))//2
         kolejne_liczby_trójkątne.append(suma)
 
-    for i in Liczby:
+    for i in suma_liczb:
         if i in kolejne_liczby_trójkątne: zlicz = zlicz + 1
 
     print('Zad4.4: ',zlicz)
